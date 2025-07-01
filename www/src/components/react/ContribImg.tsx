4610a40e3238faf4b01a742f27868c318cf942d5
dotmrjosh <dotmrjosh@sparklapse.com>
2025-07-01 12:16:10 +1000

    Working on making contrib graph an svg instead of image

--- www/src/components/react/ContribImg.tsx
+++
@@ -1,7 +0,0 @@
-export default function () {
-  return (
-    <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <p>Hello world!</p>
-    </div>
-  );
-}
